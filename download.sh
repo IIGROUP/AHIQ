4e1034600dc768fcbf6d8430964b5f383f5ea54e
weihao <xiawh3@outlook.com>
2022-04-28 12:52:32 +0100

    Update README.md

--- download.sh
+++ download.sh
@@ -1,5 +1,7 @@
 # pip install gdown
 
+save_path='checkpoints/ahiq_pipal/'
+
 # download the pretrained models
-gdown https://drive.google.com/uc?id=1Nk-IpjnDNXbWacoh3T69wkSYhYYWip2W -O checkpoints/ahiq_pipal/
-gdown https://drive.google.com/uc?id=1Jr2nLnhMA0f0uPEjMG7sH-T4WfIEasXn -O checkpoints/ahiq_pipal/
+gdown https://drive.google.com/uc?id=1Nk-IpjnDNXbWacoh3T69wkSYhYYWip2W -O $save_path
+gdown https://drive.google.com/uc?id=1Jr2nLnhMA0f0uPEjMG7sH-T4WfIEasXn -O $save_path
